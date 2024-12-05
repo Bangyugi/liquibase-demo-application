03759c7973c5b85bba6f0b604d895bc6578a5257
Bangyugi <bangtranvan08@gmail.com>
2024-12-05 09:05:23 +0700

    Initial commit

---
+++ src/main/resources/db/changelog/sql/insert-user.sql
@@ -0,0 +1,12 @@
+INSERT INTO user (username, password, email, first_name, last_name)
+VALUES
+    ('user1', 'password1', 'user1@example.com', 'John', 'Smith'),
+    ('user2', 'password2', 'user2@example.com', 'Alice', 'Johnson'),
+    ('user3', 'password3', 'user3@example.com', 'Bob', 'Williams'),
+    ('user4', 'password4', 'user4@example.com', 'Eve', 'Brown'),
+    ('user5', 'password5', 'user5@example.com', 'Charlie', 'Davis'),
+    ('user6', 'password6', 'user6@example.com', 'Daisy', 'Miller'),
+    ('user7', 'password7', 'user7@example.com', 'Michael', 'Wilson'),
+    ('user8', 'password8', 'user8@example.com', 'Sophia', 'Moore'),
+    ('user9', 'password9', 'user9@example.com', 'Daniel', 'Taylor'),
+    ('user10', 'password10', 'user10@example.com', 'Emily', 'Anderson');
